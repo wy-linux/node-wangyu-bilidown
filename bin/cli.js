7316966421644ff07ce510efa9c68dbef9251713
wangyu <2514825513@qq.com>
2023-04-07 12:06:17 +0800

    ADD: ADD error handler

--- bin/cli.js
+++ bin/cli.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
 const Download = require('../lib')
+const chalk = require('chalk')
 
 const handleRead = (chunk) => {
     process.stdin.removeListener('data', handleRead)
     process.stdout.write('\n')
-    const BiliDown = new Download(chunk.toString())
-    BiliDown.once('downloaded', () => {
-        process.stdout.write('\n\n')
-        BiliDownCli()
-    })   
+    const BiliDown = new Download(chunk.toString().replace(/[\n\r]/g, ''))
+    BiliDown.start()
+            .then((info) => {
+                // console.log(info);
+            })
+            .catch((err) => {
+                process.stdout.write(`${chalk.yellow('抓取失败！')}请输入正确的${chalk.red('网页端BiliBili视频链接')}或${chalk.green('更换视频爬取链接')}`)
+            })
+            .finally(() => {
+                process.stdout.write('\n\n')
+                BiliDownCli()
+            }) 
 }
 function BiliDownCli() {
     process.stdout.write('请输入要下载的网页端BiliBili视频链接：')
--- example/index.js
+++ example/index.js
@@ -3,6 +3,10 @@ const Download = require('../lib')
 const BiliDown = new Download(
  'https://www.bilibili.com/video/BV1HV4y177gH/?spm_id_from=333.337.search-card.all.click&vd_source=108710f9dc8bf2ee2e2257f9f77a89f7'
 )
-BiliDown.once('downloaded', () => {
-    process.stdout.write('\n视频已下载')
-}) 
+BiliDown.start()
+.then((info) => {
+    process.stdout.write(`\n${JSON.stringify(info)}`)
+})
+.catch((err) => {
+    process.stdout.write(`\n${JSON.stringify(err)}`)
+})
